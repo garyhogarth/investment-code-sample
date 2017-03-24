aab6a1a8539a919463671a4fba42b111de1851ed
Gary Hogarth <gary@garyhogarth.com>
2017-03-24 12:27:13 +0000

    Basic setup for Loan and Trenche classes

--- models/Loan.php
+++ models/Loan.php
@@ -5,5 +5,62 @@ namespace App\Models;
 
 class Loan
 {
+    private $startDate;
+    private $endDate;
+    private $tranches = [];
+
+    public function __construct()
+    {
+        // Do nothing for now
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @param \DateTime $startDate
+     */
+    public function setStartDate(\DateTime $startDate)
+    {
+        $this->startDate = $startDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @param \DateTime  $endDate
+     */
+    public function setEndDate(\DateTime $endDate)
+    {
+        $this->endDate = $endDate;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTranches()
+    {
+        return $this->tranches;
+    }
+
+    /**
+     * @param Tranche $tranche
+     * @return array
+     */
+    public function addTranche(Tranche $tranche)
+    {
+        return $this->tranches[] = $tranche;
+    }
 
 }
--- models/Tranche.php
+++ models/Tranche.php
@@ -6,4 +6,40 @@ namespace App\Models;
 class Tranche
 {
 
+    private $amount;
+    private $monthlyInterestRate;
+
+    /**
+     * @return mixed
+     */
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    /**
+     * @param mixed $amount
+     */
+    public function setAmount(float $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMonthlyInterestRate()
+    {
+        return $this->monthlyInterestRate;
+    }
+
+    /**
+     * @param mixed $monthlyInterestRate
+     */
+    public function setMonthlyInterestRate(float $monthlyInterestRate)
+    {
+        $this->monthlyInterestRate = $monthlyInterestRate;
+    }
+
+
 }
