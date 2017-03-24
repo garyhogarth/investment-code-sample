d6bbd3400818fd55e82b79cda1ef5873b2a29e93
Gary Hogarth <gary@garyhogarth.com>
2017-03-24 12:01:04 +0000

    First tests for getter/setters

--- tests/LoanTest.php
+++ tests/LoanTest.php
@@ -1,9 +1,54 @@
 <?php
 namespace App\Tests;
 
+require_once __DIR__ . '/../models/Loan.php';
+
 use PHPUnit\Framework\TestCase;
+use App\Models\Loan;
 
 class LoanTest extends TestCase
 {
+    /**
+     * @covers Loan::setStartDate()
+     */
+    public function testSetStartDateSetsCorrectly() {
+        $startDate = new \DateTime('now');
+        $endDate = new \DateTime('+ 1 week');
+
+        $loan = new Loan();
+        $loan->setStartDate($startDate);
+        $loan->setEndDate($endDate);
+
+        $this->assertEquals($startDate,$loan->getStartDate());
+        $this->assertEquals($endDate,$loan->getEndDate());
+    }
+
+    /**
+     * @covers Loan::setStartDate()
+     */
+    public function testSetStartDoesNotAllowNonDateTimeObjects() {
+        $startDate = 'test';
+
+        $this->expectException(\TypeError::class);
+
+        $loan = new Loan();
+        $loan->setStartDate($startDate);
+
+        $this->assertNotEquals($startDate,$loan->getStartDate());
+    }
+
+    /**
+     * @covers Loan::setEndDate()
+     */
+    public function testSetEndDoesNotAllowNonDateTimeObjects() {
+        $endDate = 'test';
+
+        $this->expectException(\TypeError::class);
+
+        $loan = new Loan();
+        $loan->setStartDate($endDate);
+
+        $this->assertNotEquals($endDate,$loan->getEndDate());
+    }
 
 }
