fc3c01b522059f361efa6e2457ef71d6f0b0335f
Gary Hogarth <gary@garyhogarth.com>
2017-03-24 12:19:06 +0000

    adding tests for Tranche and expanind Loan Tests

--- tests/LoanTest.php
+++ tests/LoanTest.php
@@ -2,18 +2,28 @@
 namespace App\Tests;
 
 require_once __DIR__ . '/../models/Loan.php';
+require_once __DIR__ . '/../models/Tranche.php';
 
 use PHPUnit\Framework\TestCase;
 use App\Models\Loan;
+use App\Models\Tranche;
 
 class LoanTest extends TestCase
 {
     /**
      * @covers Loan::setStartDate()
+     * @covers Loan::setEndDate()
+     * @covers Loan::addTranche()
+     * @covers Loan::getTranches()
+     * @covers Loan::getStartDate()
+     * @covers Loan::getEndDate()
      */
-    public function testSetStartDateSetsCorrectly() {
+    public function testSettersSetCorrectlyWhenGivenValidData() {
         $startDate = new \DateTime('now');
         $endDate = new \DateTime('+ 1 week');
+        $tranche1 = new Tranche();
+        $tranche2 = new Tranche();
+
 
         $loan = new Loan();
         $loan->setStartDate($startDate);
@@ -21,6 +31,13 @@ class LoanTest extends TestCase
 
         $this->assertEquals($startDate,$loan->getStartDate());
         $this->assertEquals($endDate,$loan->getEndDate());
+
+        $loan->addTranche($tranche1);
+        $this->assertEquals(1,count($loan->getTranches()));
+        $loan->addTranche($tranche2);
+        $this->assertEquals(2,count($loan->getTranches()));
+        $this->assertEquals($tranche1,$loan->getTranches()[0]);
+        $this->assertEquals($tranche2,$loan->getTranches()[1]);
     }
 
     /**
@@ -49,6 +66,20 @@ class LoanTest extends TestCase
         $loan->setStartDate($endDate);
 
         $this->assertNotEquals($endDate,$loan->getEndDate());
+    }
+
+    /**
+     * @covers Loan::addTranche()
+     */
+    public function testAddTrancheDoesNotAllowNonTrancheObjects() {
+        $tranche = 'test';
+
+        $this->expectException(\TypeError::class);
+
+        $loan = new Loan();
+        $loan->addTranche($tranche);
+
+        $this->assertEquals(0,count($loan->getTranches()));
     }
 
 }
--- tests/TrancheTest.php
+++ tests/TrancheTest.php
@@ -1,9 +1,41 @@
 <?php
 namespace App\Tests;
+require_once __DIR__ . '/../models/Tranche.php';
 
 use PHPUnit\Framework\TestCase;
+use App\Models\Tranche;
 
 class TrancheTest extends TestCase
 {
+    /**
+     * @covers Tranche::setMonthlyInterestRate()
+     * @covers Tranche::setAmount()
+     */
+    public function testSettersSetCorrectlyWhenGivenValidData() {
+        $interestRate = 0.03;
+        $amount = 1000;
+
+        $tranche = new Tranche();
+
+        $tranche->setMonthlyInterestRate($interestRate);
+        $this->assertEquals($interestRate,$tranche->getMonthlyInterestRate());
+
+        $tranche->setAmount($interestRate);
+        $this->assertEquals($amount,$tranche->getAmount());
+    }
+
+    /**
+     * @covers Tranche::setMonthlyInterestRate()
+     */
+    public function testSetMonthlyInterestRateDoesNotAllowNonFloat() {
+        $interestRate = 'test';
+
+        $this->expectException(\TypeError::class);
+
+        $tranche = new Tranche();
+        $tranche->setMonthlyInterestRate($interestRate);
+
+        $this->assertNotEquals($interestRate,$tranche->getMonthlyInterestRate());
+    }
 
 }
